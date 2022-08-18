89399438c659d810f9e928bfe485b6d30bf031fe
Huy Nguyễn <huynqhe130759@fpt.edu.vn>
2022-08-19 02:44:53 +0700

    Create New Files
    Up thêm file HTML và sql các câu lệnh Query

---
+++ Cau_Lenh_Query.sql
@@ -0,0 +1,9 @@
+﻿
+			
+Select * from employee											/* Lấy được eid */
+
+SELECT* FROM table ngày công WHERE MONTH( ngày) = 8 and eid = ? /* Lấy được ngày công id */
+
+select * from table chấm công where ngày công id = ?			/* lấy được chấm công type */
+
+select * from ngày công detail where chấm công type = ?			/* lấy được info ca sáng hay tối cho vào text  */
