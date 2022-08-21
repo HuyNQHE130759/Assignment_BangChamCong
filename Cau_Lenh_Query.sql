674e8502baf21a66494d03ed4b0403388a64c9a9
Huy Nguyễn <huynqhe130759@fpt.edu.vn>
2022-08-21 11:40:53 +0700

    Update Query Codes
    Cập Nhật Câu Lệnh Query

--- Cau_Lenh_Query.sql
+++ Cau_Lenh_Query.sql
@@ -1,9 +1,19 @@
-﻿
+﻿Select * from Employee
 			
-Select * from employee											/* Lấy được eid */
+Select * from Employee where eid = 24											/* Lấy được eid */
 
 SELECT* FROM table ngày công WHERE MONTH( ngày) = 8 and eid = ? /* Lấy được ngày công id */
 
+SELECT* FROM workingDate WHERE MONTH(date) = 8
+
+SELECT* FROM workingDate WHERE MONTH(date) = 8 and eid = 24
+
 select * from table chấm công where ngày công id = ?			/* lấy được chấm công type */
+SELECT * FROM Working_Table
+
+SELECT * FROM Working_Table where workingdate_id = 3
 
 select * from ngày công detail where chấm công type = ?			/* lấy được info ca sáng hay tối cho vào text  */
+SELECT * FROM workingDate_Detail
+
+SELECT * FROM workingDate_Detail where Timekeep_type = 1
